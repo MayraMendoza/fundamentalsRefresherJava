ab3893e755df8bdb1a2b95dbaf36f7d90a4caeb6
MayraMendoza <mayra601m@gmail.com>
2022-09-13 20:09:07 -0400

    data types

---
+++ src/com/fundamentalsRefresher/DataTypes.java
@@ -0,0 +1,35 @@
+package com.fundamentalsRefresher;
+
+import java.util.Date;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+        // we use variable to temporarily store data in computer memory location.
+
+        // when assigning a variable
+        // type of variable,  name/label/identifier assignment operator= and initial value.
+        int myAge = 30;
+
+        // you can also copy the value of one variable into another.
+        int herAge = myAge;
+        myAge =15;
+
+        System.out.println(herAge);
+
+        /**
+         * reference types - used to store complex objects - like data and mail messages
+         */
+//        Date now = new Date();
+//        // Date is a class in java.util package
+//        // now- is an instance of the Date class
+//        // new is allocating data for this variable.
+//        System.out.println(now);
+        /**
+         *
+         */
+
+
+    }
+
+}
